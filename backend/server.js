0a1f5347452e8d7cd9686439b27a4cc1b13a5d04
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-08-01 20:29:06 +0530

    mongodb connected . next move to set up the models

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import dotenv from 'dotenv'
 dotenv.config()
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 const port = process.env.PORT || 4000
 import userRoutes from './routes/userRoutes.js'
+
+connectDB();
 
 const app = express()
 
