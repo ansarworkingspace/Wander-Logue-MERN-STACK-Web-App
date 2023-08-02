4c816839fc1fcae0fe0241e83cf9d1ae62190c46
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-08-02 12:24:59 +0530

    now i set up frontend components and screens.now just create basic UI of register and login.also i set the router of register and login.next i move to set up redux and redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './Screen/HomeScreen.jsx';
+import LoginScreen from './Screen/LoginScreen.jsx';
+import RegisterScreen from './Screen/RegisterScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+     
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+      <Route path='/register' element={<RegisterScreen />} /> 
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
