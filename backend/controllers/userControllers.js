181a016128240c74d6b39fdc84f1106302d2402b
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-08-02 09:25:57 +0530

    user models created and tested in postman , working good.and now moved to make JWT token .

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,12 +1,44 @@
 import asyncHandler from 'express-async-handler'
-
+import User from '../models/userModels.js'
 
 const authUser = asyncHandler(async (req,res)=>{
     res.status(200).json({message:"auth user"})
 });
 
 const registerUser = asyncHandler(async (req,res)=>{
-    res.status(200).json({message:"Register user"})
+    // console.log(req.body);
+    const {name,email,password,mobile} = req.body;
+    const userExists = await User.findOne({email:email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        mobile
+    });
+
+    if(user){
+        // generateToken(res,user._id)
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            mobile:user.mobile
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid user data');
+    }
+
+    
+    
+    
+   
 });
 
 const logoutUser = asyncHandler(async (req,res)=>{
