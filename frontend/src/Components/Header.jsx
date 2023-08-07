12f398fa0a193b15f051c110ea39824d43175a73
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-08-07 11:37:26 +0530

    some design completed in home of user

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,23 +1,20 @@
-// import { useEffect,useState } from 'react';
-// import { Navbar, Nav, Container,NavDropdown, Badge  } from 'react-bootstrap';
+
+// import React from 'react';
+// import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
 // import { LinkContainer } from 'react-router-bootstrap';
-// import { FaSignInAlt, FaSignOutAlt, FaGoogle, FaPlus } from 'react-icons/fa'
+// import { FaPlus, FaGoogle } from 'react-icons/fa';
 // import { useSelector, useDispatch } from 'react-redux';
 // import { useNavigate } from 'react-router-dom';
 // import { useLogoutMutation } from '../slices/UserApiSlice';
 // import { logout } from '../slices/AuthSlice';
-
-
-
+// import '../css/header.css';
 
 // const Header = () => {
 //   const { userInfo } = useSelector((state) => state.auth);
-
 //   const dispatch = useDispatch();
 //   const navigate = useNavigate();
-
 //   const [logoutApiCall] = useLogoutMutation();
-  
+
 //   const logoutHandler = async () => {
 //     try {
 //       await logoutApiCall().unwrap();
@@ -28,14 +25,12 @@
 //     }
 //   };
 
-
-
 //   return (
 //     <header>
 //       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect className='custom-navbar'>
 //         <Container>
 //           <LinkContainer to='/'>
-//             <Navbar.Brand className='navbar-brand-custom' style={{ fontFamily: 'Londrina Solid', fontSize: "2rem", color: "white" }}>WANDER-LOGUE</Navbar.Brand>
+//             <Navbar.Brand className='navbar-brand-custom'>WANDER-LOGUE</Navbar.Brand>
 //           </LinkContainer>
 //           <Navbar.Toggle aria-controls='basic-navbar-nav' />
 //           <Navbar.Collapse id='basic-navbar-nav'>
@@ -45,6 +40,13 @@
 //                   <Nav.Link>
 //                     <FaPlus />
 //                   </Nav.Link>
+//                   <div className="dp-container">
+//                     {userInfo.profileImage ? (
+//                       <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
+//                     ) : (
+//                       <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
+//                     )}
+//                   </div>
 //                   <NavDropdown title={userInfo.name} id='username'>
 //                     <LinkContainer to='/profile'>
 //                       <NavDropdown.Item>Profile</NavDropdown.Item>
@@ -75,6 +77,87 @@
 // };
 
 // export default Header;
+
+
+// import React from 'react';
+// import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
+// import { LinkContainer } from 'react-router-bootstrap';
+// import { FaPlus, FaGoogle } from 'react-icons/fa';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { useNavigate } from 'react-router-dom';
+// import { useLogoutMutation } from '../slices/UserApiSlice';
+// import { logout } from '../slices/AuthSlice';
+// import '../css/header.css';
+
+// const Header = () => {
+//   const { userInfo } = useSelector((state) => state.auth);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const [logoutApiCall] = useLogoutMutation();
+
+//   const logoutHandler = async () => {
+//     try {
+//       await logoutApiCall().unwrap();
+//       dispatch(logout());
+//       navigate('/login');
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   return (
+//     <header>
+//       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect className='custom-navbar'>
+//         <Container>
+//           <LinkContainer to='/'>
+//             <Navbar.Brand className='navbar-brand-custom'>WANDER-LOGUE</Navbar.Brand>
+//           </LinkContainer>
+//           <Navbar.Toggle aria-controls='basic-navbar-nav' />
+//           <Navbar.Collapse id='basic-navbar-nav'>
+//             <Nav className='ms-auto'>
+//               {userInfo ? (
+//                 <>
+//                   <Nav.Link>
+//                     <FaPlus />
+//                   </Nav.Link>
+//                   <div className="dp-container">
+//                     {userInfo.profileImage ? (
+//                       <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
+//                     ) : (
+//                       <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
+//                     )}
+//                   </div>
+//                   <NavDropdown title="Profile" id='username'>
+//                     <LinkContainer to='/profile'>
+//                       <NavDropdown.Item>Profile</NavDropdown.Item>
+//                     </LinkContainer>
+//                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+//                   </NavDropdown>
+//                 </>
+//               ) : (
+//                 <>
+//                   <LinkContainer to='/login'>
+//                     <Nav.Link>
+//                       <FaGoogle /> Sign In
+//                     </Nav.Link>
+//                   </LinkContainer>
+//                   <LinkContainer to='/register'>
+//                     <Nav.Link>
+//                       <FaGoogle /> Sign Up
+//                     </Nav.Link>
+//                   </LinkContainer>
+//                 </>
+//               )}
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
 
 import React from 'react';
 import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
@@ -114,17 +197,19 @@ const Header = () => {
             <Nav className='ms-auto'>
               {userInfo ? (
                 <>
-                  <Nav.Link>
-                    <FaPlus />
-                  </Nav.Link>
                   <div className="dp-container">
+                    <div className="plus-icon">
+                      <Nav.Link>
+                        <FaPlus />
+                      </Nav.Link>
+                    </div>
                     {userInfo.profileImage ? (
                       <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
                     ) : (
                       <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
                     )}
                   </div>
-                  <NavDropdown title={userInfo.name} id='username'>
+                  <NavDropdown title="Profile" id='username'>
                     <LinkContainer to='/profile'>
                       <NavDropdown.Item>Profile</NavDropdown.Item>
                     </LinkContainer>
@@ -154,3 +239,4 @@ const Header = () => {
 };
 
 export default Header;
+
