2c2655db7e8596cb566531d003bc2ea88cf95cc9
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-08-17 22:39:03 +0530

    like color changing ui is set up , next move build that functionality

--- frontend/src/Screen/ViewBlog.jsx
+++ frontend/src/Screen/ViewBlog.jsx
@@ -1,92 +1,5 @@
 
 
-
-
-// import { useParams } from 'react-router-dom';
-// import { useEffect, useState } from 'react'; // Import the useState and useEffect hooks
-// import { Image } from 'react-bootstrap';
-// import { FaThumbsUp, FaBookmark, FaComment } from 'react-icons/fa'; 
-// import '../css/viewBlog.css';
-// const ViewBlog = () => {
-//   const { blogId } = useParams();
-//   const [selectedBlog, setSelectedBlog] = useState(null); // State to hold the selected blog details
-
-//   useEffect(() => {
-//     // Fetch the selected blog details using the provided blogId
-//     fetch(`http://localhost:4000/api/users/getOneBlog/${blogId}`) // Adjust the API route accordingly
-//       .then((response) => response.json())
-//       .then((data) => setSelectedBlog(data))
-//       .catch((error) => console.error(error));
-//   }, [blogId]); // Fetch when blogId changes
-
-//   if (!selectedBlog) {
-//     return <p>Loading...</p>;
-//   }
-
-//   return (
-//     <div className="viewBlog-container">
-
-//       <div className='titleView'>
-//       <h2>{selectedBlog.title}</h2>
-//       </div>
-
-//       <div className='blogDetails'>
-//       <p>Author: {selectedBlog.author.name}</p>
-//       <p>Created on: {new Date(selectedBlog.createdAt).toLocaleDateString()}</p>
-//       </div>
-
-
-//       <div className='proLine'></div>
-
-//       <div className='actionDetails'>
-//       <div className="iconWrapper">
-//           <FaThumbsUp />
-//         </div>
-//         <div className="iconWrapper">
-//           <FaBookmark />
-//         </div>
-//         <div className="iconWrapper">
-//           <FaComment />
-//         </div>
-//       </div>
-
-//       <div className='proLine'></div>
-
-//       <div className='imageView'>
-//         {selectedBlog.images.length > 0 && (
-//         <Image
-//           src={`http://localhost:4000/api/users/${selectedBlog.images[0]}`}
-//           alt='Blog'
-//           className='viewImageOndiv'
-//         />
-//         )}
-//       </div>
-
-
-//       <div className='summaryDetails'>
-//       <p>{selectedBlog.summary}</p>
-//       </div>
-
-//       <div className='proLine'></div>
-
-//       <div className='contentView' dangerouslySetInnerHTML={{ __html: selectedBlog.content }} />
-      
-      
-     
-      
-     
-//     </div>
-//   );
-// };
-
-// export default ViewBlog;
-
-
-
-
-
-
-
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react'; // Import the useState and useEffect hooks
@@ -97,7 +10,7 @@ const ViewBlog = () => {
   const { blogId } = useParams();
  
   const [selectedBlog, setSelectedBlog] = useState(null); // State to hold the selected blog details
-
+  const [liked, setLiked] = useState(false); // State to manage like/unlike
 
 
 
@@ -128,17 +41,10 @@ const ViewBlog = () => {
   }, [blogId]); // Fetch when blogId changes orginal
 
 
-  // useEffect(() => {
-  //   fetch(`http://localhost:4000/api/users/getOneBlog/${blogId}`)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log("Fetched data:", data); // Add this line
-  //       setSelectedBlog(data);
-  //     })
-  //     .catch((error) => console.error(error));
-  // }, [blogId]);
 
-  
+  const handleLikeClick = () => {
+    setLiked(!liked);
+  };
 
 
   if (!selectedBlog) {
@@ -157,12 +63,23 @@ const ViewBlog = () => {
       <p>Created on: {new Date(selectedBlog.createdAt).toLocaleDateString()}</p>
       </div>
 
-
       <div className='proLine'></div>
 
       <div className='actionDetails'>
-      <div className="iconWrapper">
+      <div
+          className="iconWrapper"
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            color: liked ? '#f93131' : 'white', // Change color based on liked state
+            cursor: 'pointer',
+          }}
+          onClick={handleLikeClick} // Call handleLikeClick on click
+        >
           <FaThumbsUp />
+          <h4  style={{color:"white", fontSize: "0.8rem", marginLeft: "0.7rem", marginTop: "0.2rem" }}>
+           500
+          </h4>
         </div>
         <div className="iconWrapper" onClick={handleBookmarkClick}>
           <FaBookmark />
