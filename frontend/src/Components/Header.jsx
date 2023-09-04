3865899c638b0b171015ec94a439d80fdd482e2f
Muhammed_Ansar <iammuhammedansar@gmail.com>
2023-09-04 18:36:27 +0530

    notification get on header - testing one

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,36 +1,35 @@
 
+
 // import React from 'react';
 // import { useEffect } from 'react';
 // import { useLocation } from 'react-router-dom';
-// import { Navbar, Nav, Container, NavDropdown, Image } from 'react-bootstrap';
+// import { Navbar, Nav, Container, NavDropdown, Image, Button } from 'react-bootstrap';
 // import { LinkContainer } from 'react-router-bootstrap';
 // import { Link } from 'react-router-dom';
 // import { FaPlus, FaGoogle,FaHome ,FaComment, FaUser } from 'react-icons/fa';
 // import { useSelector, useDispatch } from 'react-redux';
+// import { setCredentials } from '../slices/AuthSlice';
 // import { useNavigate } from 'react-router-dom';
 // import { useLogoutMutation } from '../slices/UserApiSlice';
 // import { logout } from '../slices/AuthSlice';
 // import { useCookies } from 'react-cookie'; // Import the hook
 // import '../css/header.css';
-// import jwt_decode from "jwt-decode";
-
-
+// import { toast } from 'react-toastify';
 
 // const Header = () => {
 //   const { userInfo } = useSelector((state) => state.auth);
 //   const dispatch = useDispatch();
 //   const navigate = useNavigate();
-//   const [logoutApiCall] = useLogoutMutation();
-//   const [cookies] = useCookies(['jwt']);
 //   const location = useLocation();
-
+  
 //   const isLandingRoute = location.pathname === '/landing';
 
-
+ 
+  
 
 //   return (
 //     <header>
-//       <Navbar style={{ backgroundColor: '#181a1b' }} variant='dark' expand='lg' collapseOnSelect className='custom-navbar'>
+//       <Navbar style={{ backgroundColor: '#055049' }} variant='dark' expand='lg' collapseOnSelect className='custom-navbar'>
 //         <Container>
 //           <LinkContainer to='/'>
 //             <Navbar.Brand className='navbar-brand-custom'>WANDER<span style={{color:"#e8f32b"}}>LOGUE</span></Navbar.Brand>
@@ -48,7 +47,7 @@
 //                   {userInfo && (
         
 //         <div className="home-icon" onClick={() => navigate('/')}>
-//           <Nav.Link>
+//           <Nav.Link style={{color:"white"}}>
 //             <FaHome />
 //           </Nav.Link>
 //         </div>
@@ -57,16 +56,16 @@
 
 
 // {userInfo && (
-//       <div className="home-icon" onClick={() => navigate('/chat')}>
-//         <Nav.Link>
+//       <div className="home-icon" onClick={() => navigate('/ChatRoom')}>
+//         <Nav.Link  style={{color:"white"}}>
 //           <FaComment />
 //         </Nav.Link>
 //       </div>
 //     )}
 
 //     {userInfo && (
-//       <div className="home-icon" onClick={() => navigate('/profile')}>
-//         <Nav.Link>
+//       <div className="home-icon" onClick={() => navigate('/allFollowing')}>
+//         <Nav.Link   style={{color:"white"}}>
 //           <FaUser />
 //         </Nav.Link>
 //       </div>
@@ -78,20 +77,27 @@
 //       {userInfo && (
         
 //         <div className="plus-icon" onClick={() => navigate('/create')}>
-//           <Nav.Link>
+//           <Nav.Link style={{color:"white"}}>
 //             <FaPlus />
 //           </Nav.Link>
 //         </div>
 //       )}
 //     </Nav>
-//                     {userInfo.profileImage ? (
-//                       <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
-//                     ) : (
-//                       <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
-//                     )}
+                
+
+
+
+// {userInfo.profileImage ? (
+//     <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
+//   ) : userInfo.profileGoogleImage ? (
+//     <Image src={userInfo.profileGoogleImage} alt="Google Display Picture" className="dp-image rounded-circle" />
+//   ) : (
+//     <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
+//   )}
+
 //                   </div>
-//                   <LinkContainer style={{ fontFamily: "Sora", fontSize: "0.9rem",marginLeft:"0.2rem",marginTop:"0.2rem",color:"#e6e1e1" }} to='/profile'>
-//   <Nav.Link>
+//                   <LinkContainer style={{ fontFamily: "Sora", fontSize: "0.9rem",marginLeft:"0.2rem",marginTop:"0.2rem",color:"rgb(255 255 255)" }} to='/profile'>
+//   <Nav.Link >
 //     Profile
 //   </Nav.Link>
 // </LinkContainer>
@@ -109,16 +115,11 @@
 //                     </LinkContainer>
 //                   ) : (
 //                     <>
-//                       <LinkContainer style={{ fontFamily: "Sora", fontSize: "0.8rem" }} to='/login'>
-//                         <Nav.Link>
-//                           <FaGoogle /> Sign In
-//                         </Nav.Link>
-//                       </LinkContainer>
-//                       <LinkContainer style={{ fontFamily: "Sora", fontSize: "0.8rem" }} to='/register'>
-//                         <Nav.Link>
-//                           <FaGoogle /> Sign Up
-//                         </Nav.Link>
-//                       </LinkContainer>
+                    
+                         
+
+                     
+
 //                     </>
 //                   )}
 //                 </>
@@ -136,10 +137,11 @@
 
 
 
+//testing real time notification
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Navbar, Nav, Container, NavDropdown, Image, Button } from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown, Image, Button ,Badge } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 import { FaPlus, FaGoogle,FaHome ,FaComment, FaUser } from 'react-icons/fa';
@@ -152,6 +154,8 @@ import { useCookies } from 'react-cookie'; // Import the hook
 import '../css/header.css';
 import { toast } from 'react-toastify';
 
+import io from 'socket.io-client';
+
 const Header = () => {
   const { userInfo } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -160,8 +164,64 @@ const Header = () => {
   
   const isLandingRoute = location.pathname === '/landing';
 
+  const [hasUnreadMessages, setHasUnreadMessages] = useState(false); // State for unread messages
+  const socket = io('http://localhost:4000'); 
+  const [currentChatRoomId, setCurrentChatRoomId] = useState(null); // Define currentChatRoomId
+  const [socketConnected, setSocketConnected] = useState(false);
+
+
+
+  useEffect(()=>{
  
-  
+    socket.emit("setup",userInfo)
+    socket.on("connected",()=>setSocketConnected(true))
+
+
+
+//  // Cleanup function to leave the socket room when the component unmounts
+//  return () => {
+//   socket.off("setup",userInfo._id);
+//   // socket.emit("leaveRoom", { room: chatRoomId }); // Emit a leave event
+//   socket.disconnect(); // Disconnect the socket connection
+// };
+
+ },[])
+ 
+
+
+
+  useEffect(() => {
+    // Listen for message events and update hasUnreadMessages
+    socket.on('message received', (newMessageReceived) => {
+      if (newMessageReceived.sender._id !== userInfo._id) {
+        setHasUnreadMessages(true);
+      }
+    });
+    
+    // Listen for new message notification events and update hasUnreadMessages
+    socket.on('new message notification', (newMessageReceived) => {
+      if (
+        currentChatRoomId !== newMessageReceived.room._id &&
+        newMessageReceived.sender._id !== userInfo._id
+      ) {
+        setHasUnreadMessages(true);
+      }
+    });
+
+    // return () => {
+    //   // Clean up socket.io event listeners
+    //   socket.off('message received');
+    //   socket.off('new message notification');
+    // };
+  }, [userInfo._id]);
+
+
+
+
+
+
+
+
 
   return (
     <header>
@@ -194,7 +254,8 @@ const Header = () => {
 {userInfo && (
       <div className="home-icon" onClick={() => navigate('/ChatRoom')}>
         <Nav.Link  style={{color:"white"}}>
-          <FaComment />
+        <FaComment />
+        {hasUnreadMessages && <h5>new messages</h5>}
         </Nav.Link>
       </div>
     )}
@@ -219,11 +280,7 @@ const Header = () => {
         </div>
       )}
     </Nav>
-                    {/* {userInfo.profileImage ? (
-                      <Image src={`http://localhost:4000/api/users/uploads/${userInfo.profileImage}`} alt="Display Picture" className="dp-image rounded-circle" />
-                    ) : (
-                      <div className="dp-initials">{userInfo.name ? userInfo.name.charAt(0).toUpperCase() : ''}</div>
-                    )} */}
+                
 
 
 
